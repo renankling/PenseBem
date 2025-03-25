24e7e08281e08a6f7edee0948f5a78a036b84e61
renankling <renankling00@gmail.com>
2025-03-25 00:50:38 -0300

    Gabarito 084

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gabaritos = {
-    "081": ["D", "A", "A","D","C","B","B","C","A","D","B","A","D"], //parei na questao 13, seguir da 14 adiante
+    "081": ["D", "A", "A","D","C","B","B","C","A","D","B","A","D","C","C","B","A","B","D","A",
+            "C","A","B","C","B","D","A","B","A","B"],
     "082": ["C"],
     "083": [],
-    "084": []
+    "084": ["C","C","C","B","A","D","B","A","D","B","B","D","C","A","A","A","B","D","A","B","C",
+            "C","D","D","C","C","D","D","A","A"]
 };
 
 let codigo;
@@ -67,6 +69,7 @@ function proximaPergunta() {
 function atualizarTela() {
     document.getElementById("pergunta-numero").textContent = `Pergunta: ${indicePergunta + 1}`;
     document.getElementById("tentativas-restantes").textContent = `Tentativas restantes: ${tentativas}`;
+    document.getElementById("pontos").textContent = `Seus pontos: ${pontuacao}`;
 }
 
 iniciarQuiz();
